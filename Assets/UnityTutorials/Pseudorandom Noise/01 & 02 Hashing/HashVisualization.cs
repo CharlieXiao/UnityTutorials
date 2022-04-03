917b4abee8dfb6dfb5b4cb0f313ceab81dc6d14f
CharlieXiao <1227609725@qq.com>
2022-04-03 14:26:19 +0800

    Finish Hashing 2
    1. manually vectorized hash job
    2. make hash visualized on any 3D shape defined by implicit function
    3. implicit function for OctahedronSphere
    4. apply normal transform and normal displacement

--- Assets/UnityTutorials/Pseudorandom Noise/01 & 02 Hashing/HashVisualization.cs
+++ Assets/UnityTutorials/Pseudorandom Noise/01 & 02 Hashing/HashVisualization.cs
@@ -9,38 +9,13 @@ namespace UnityTutorials.Pseudorandom_Noise._01_Hashing
 {
     public class HashVisualization : MonoBehaviour
     {
-        [BurstCompile(FloatPrecision.Standard, FloatMode.Fast, CompileSynchronously = true)]
-        struct HashJob : IJobFor
-        {
-            [WriteOnly] public NativeArray<uint> hashes;
-
-            [ReadOnly] public NativeArray<float3> positions;
-
-            [ReadOnly] public SmallXXHash hash;
-            [ReadOnly] public float3x4 domainTRS;
-
-            public void Execute(int i)
-            {
-                float3 p = mul(domainTRS,float4(positions[i],1.0f));
-                
-                // [vf,uf] is [-0.5,0.5] x [-0.5,0.5]
-                int u = (int)floor(p.x);
-                int v = (int)floor(p.y);
-                int w = (int)floor(p.z);
-                hashes[i] = hash.Eat(u).Eat(v).Eat(w);
-            }
-        }
-
-        [SerializeField] private SpaceTRS domain = new SpaceTRS
-        {
-            scale = 8.0f
-        };
-
         private static int 
             hashedId = Shader.PropertyToID("_Hashes"),
             configId = Shader.PropertyToID("_Config"),
             positionId = Shader.PropertyToID("_Positions"),
             normalId = Shader.PropertyToID("_Normals");
+        
+        [SerializeField] private SpaceTRS domain = new SpaceTRS { scale = 8.0f };
 
         [SerializeField] private Mesh instanceMesh;
 
@@ -51,17 +26,17 @@ namespace UnityTutorials.Pseudorandom_Noise._01_Hashing
         [SerializeField, Range(1, 512)] private int resolution;
         
         // move the cube along normal
-        [SerializeField, Range(-2.0f, 2.0f)] private float displacement = 0.1f;
+        [SerializeField, Range(-0.5f, 0.5f)] private float displacement = 0.1f;
         
         [SerializeField, Range(0.1f, 10f)] private float instanceScale = 1.0f;
 
+        [SerializeField] private Shape shape = Shape.Plane;
+        
         private float invResolution;
 
-        private NativeArray<uint> hashes;
+        private NativeArray<uint4> hashes;
 
-        private NativeArray<float3> positions;
-
-        private NativeArray<float3> normals;
+        private NativeArray<float3x4> positions,normals;
 
         // transfer data to shader
         private ComputeBuffer hashesBuffer,positionsBuffer,normalsBuffer;
@@ -72,18 +47,68 @@ namespace UnityTutorials.Pseudorandom_Noise._01_Hashing
         private bool updatePosition = false;
 
         private Bounds bounds;
+        
+        private enum Shape
+        {
+            Plane,UVSphere,Torus,OctahedronSphere
+        }
+
+        private static Shapes.ScheduleDelegate[] shapeJobs =
+        {
+            Shapes.Job<Shapes.Plane>.ScheduleParallel,
+            Shapes.Job<Shapes.UVSphere>.ScheduleParallel,
+            Shapes.Job<Shapes.Torus>.ScheduleParallel,
+            Shapes.Job<Shapes.OctahedronSphere>.ScheduleParallel
+        };
+
+        [BurstCompile(FloatPrecision.Standard, FloatMode.Fast, CompileSynchronously = true)]
+        struct HashJob : IJobFor
+        {
+            [WriteOnly] public NativeArray<uint4> hashes;
+
+            [ReadOnly] public NativeArray<float3x4> positions;
+
+            [ReadOnly] public SmallXXHash4 hash;
+            [ReadOnly] public float3x4 domainTRS;
+
+            float4x3 TransformPositions(float3x4 trs, float4x3 p)
+            {
+                return float4x3(
+                    // manually apply the transform and utilize the SIMD instructions
+                    trs.c0.x * p.c0 + trs.c1.x * p.c1 + trs.c2.x * p.c2 + trs.c3.x,
+                    trs.c0.y * p.c0 + trs.c1.y * p.c1 + trs.c2.y * p.c2 + trs.c3.y,
+                    trs.c0.z * p.c0 + trs.c1.z * p.c1 + trs.c2.z * p.c2 + trs.c3.z
+                );
+            }
+
+            public void Execute(int i)
+            {
+                // our target is float3x4, transform matrix is float3x4, so we need to transpose it
+                float4x3 p = TransformPositions(domainTRS,transpose(positions[i]));
+                
+                // [vf,uf] is [-0.5,0.5] x [-0.5,0.5]
+                int4 u = (int4)floor(p.c0);
+                int4 v = (int4)floor(p.c1);
+                int4 w = (int4)floor(p.c2);
+                hashes[i] = hash.Eat(u).Eat(v).Eat(w);
+            }
+        }
 
         private void OnEnable()
         {
             int length = resolution * resolution;
+            // make vector work for odd length
+            length = length / 4 + (length & 1);
             
-            hashes = new NativeArray<uint>(length, Allocator.Persistent);
-            positions = new NativeArray<float3>(length, Allocator.Persistent);
-            normals = new NativeArray<float3>(length, Allocator.Persistent);
+            // CPU buffer
+            hashes = new NativeArray<uint4>(length, Allocator.Persistent);
+            positions = new NativeArray<float3x4>(length, Allocator.Persistent);
+            normals = new NativeArray<float3x4>(length, Allocator.Persistent);
 
-            hashesBuffer = new ComputeBuffer(length, 4);
-            positionsBuffer = new ComputeBuffer(length, 3 * 4);
-            normalsBuffer = new ComputeBuffer(length, 3 * 4);
+            // GPU buffer, the size is still uint,float3 and float3
+            hashesBuffer = new ComputeBuffer(length*4, 4);
+            positionsBuffer = new ComputeBuffer(length*4, 3 * 4);
+            normalsBuffer = new ComputeBuffer(length*4, 3 * 4);
             
             invResolution = 1.0f / resolution;
             updatePosition = true;
@@ -121,9 +146,9 @@ namespace UnityTutorials.Pseudorandom_Noise._01_Hashing
                 updatePosition = false;
                 transform.hasChanged = false;
                 // update position
-                JobHandle handle = Shapes.Job.ScheduleParallel(positions,normals, resolution,transform.localToWorldMatrix, default);
+                JobHandle handle = shapeJobs[(int)shape](positions,normals, resolution,transform.localToWorldMatrix, default);
 
-                new Bounds(
+                bounds = new Bounds(
                     transform.position,
                     float3(2.0f * cmax(abs(transform.lossyScale)) + displacement)
                 );
@@ -131,28 +156,29 @@ namespace UnityTutorials.Pseudorandom_Noise._01_Hashing
                 // 执行Burst任务（并行）
                 new HashJob
                 {
-                    hashes = hashes,
                     positions = positions,
-                    hash = SmallXXHash.Seed(seed),
+                    hashes = hashes,
+                    hash = SmallXXHash4.Seed(seed),
                     domainTRS = domain.Matrix
+                    // the resolution must be even
                 }.ScheduleParallel(hashes.Length, resolution, handle).Complete();
             
-                // copy data to buffer
-                hashesBuffer.SetData(hashes);
-                positionsBuffer.SetData(positions);
-                normalsBuffer.SetData(normals);
+                // copy data from CPU to GPU
+                hashesBuffer.SetData(hashes.Reinterpret<uint>(4 * 4));
+                positionsBuffer.SetData(positions.Reinterpret<float3>(3*4*4));
+                normalsBuffer.SetData(normals.Reinterpret<float3>(3*4*4));
                 
                 propertyBlock.SetBuffer(hashedId, hashesBuffer);
                 propertyBlock.SetBuffer(positionId,positionsBuffer);
                 propertyBlock.SetBuffer(normalId,normalsBuffer);
                 // config 存储了resolution和resolution的倒数，在shader中使用
                 propertyBlock.SetVector(configId, new Vector4(
-                    resolution, instanceScale * invResolution,displacement * invResolution));
+                    resolution, instanceScale * invResolution,displacement));
             }
             // 直接绘制即可
             Graphics.DrawMeshInstancedProcedural(
                 instanceMesh, 0, material, 
-                bounds, hashes.Length, propertyBlock);
+                bounds, resolution * resolution, propertyBlock);
         }
     }
 }
--- Assets/UnityTutorials/Pseudorandom Noise/01 & 02 Hashing/SmallXXHash.cs
+++ Assets/UnityTutorials/Pseudorandom Noise/01 & 02 Hashing/SmallXXHash.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using Unity.Mathematics;
+
 namespace UnityTutorials.Pseudorandom_Noise._01_Hashing
 {
     public readonly struct SmallXXHash
@@ -42,5 +43,48 @@ namespace UnityTutorials.Pseudorandom_Noise._01_Hashing
         public SmallXXHash Eat(byte data) => RotateLeft(accumulator + data * primeE, 11) * primeA;
 
         public static SmallXXHash Seed(int seed) => (uint)seed + primeE;
+    }
+    
+    public readonly struct SmallXXHash4
+    {
+        const uint primeB = 0b10000101111010111100101001110111;
+        const uint primeC = 0b11000010101100101010111000111101;
+        const uint primeD = 0b00100111110101001110101100101111;
+        const uint primeE = 0b00010110010101100110011110110001;
+
+        private readonly uint4 accumulator;
+
+        public SmallXXHash4(uint4 accumulator)
+        {
+            this.accumulator = accumulator;
+        }
+        
+        // uint -> SmallXXHash
+
+        public static implicit operator SmallXXHash4(uint4 accumulator) => new SmallXXHash4(accumulator);
+
+        // SmallXXHash -> uint
+        public static implicit operator uint4(SmallXXHash4 hash)
+        {
+            uint4 avalanche = hash.accumulator;
+            avalanche ^= avalanche >> 15;
+            avalanche *= primeB;
+            avalanche ^= avalanche >> 13;
+            avalanche *= primeC;
+            avalanche ^= avalanche >> 16;
+            return avalanche;
+        }
+        
+        // SmallXXHash -> SmallXXHash4
+        public static implicit operator SmallXXHash4(SmallXXHash hash) => new SmallXXHash4((uint)hash);
+        
+        // 滚动哈希，将左移的数据放到数据的最后
+        // 11001100 << 3 | 11001100 >> 5
+        // 01100110
+        static uint4 RotateLeft(uint4 data, int steps) => (data << steps) | (data >> (32-steps));
+
+        public SmallXXHash4 Eat(int4 data) => RotateLeft(accumulator + (uint4)data * primeC,17) * primeD;
+
+        public static SmallXXHash4 Seed(int4 seed) => (uint4)seed + primeE;
     }
 }
